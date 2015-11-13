7ebe46c07a5fc138af33e5277603dddeccb72b8d
David Chaloupka <chaloupka.david@gmail.com>
2015-11-13 21:50:47 +0100

    Got rid of web dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,6 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.3.1"]
-                 [instaparse "1.4.1"] ;; needed to prevent arity exception on StringReader
-                 [ring/ring-defaults "0.1.2"]
                  [com.draines/postal "1.11.3"]]
-  :plugins [[lein-ring "0.8.13"]]
   :main web-watchdog.core
-  :aot [web-watchdog.core]
-  :ring {:handler web-watchdog.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :aot [web-watchdog.core])
