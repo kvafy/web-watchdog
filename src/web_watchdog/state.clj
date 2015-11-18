59c843670bd1908c1a89b17486aac908a3dbbd26
David Chaloupka <chaloupka.david@gmail.com>
2015-11-19 00:35:08 +0100

    web-watchdog.state namespace was accidentally excluded from version control

---
+++ src/web_watchdog/state.clj
@@ -0,0 +1,53 @@
+(ns web-watchdog.state
+  (:require [web-watchdog.core :as core]
+            [web-watchdog.persistence :as persistence]
+            [web-watchdog.networking :as networking]
+            [web-watchdog.utils :as utils]))
+
+(defonce app-state (atom nil))
+
+(def default-state
+  {:sites [(comment
+             ;; This is how a site is represented.
+             {:title      "European LISP Symposium"
+              :url        "http://www.european-lisp-symposium.org"
+              :re-pattern #"(?s).*"
+              :emails     ["happy@lisper.com"]
+              :state      {:last-check-utc nil
+                           :content-hash   nil
+                           :fail-counter   0
+                           :last-error-msg nil}})]
+   ;; Global configuration.
+   :config {:check-interval-ms (* 1000 60 60)}})
+
+
+;; state change listeners
+
+(defn notify-by-email! [old-state new-state]
+  (dorun
+    (map (fn [[old-site new-site]]
+           (when-let [change-type (core/site-change-type old-site new-site)]
+             (utils/log (format "Change of type %s detected at [%s]" change-type (:title new-site)))
+             (networking/notify-site-changed! new-site change-type)))
+         ; filter out change of type "site added/removed from watched sites list"
+         (core/common-sites old-state new-state))))
+
+(defn persist-new-state! [old-state new-state]
+  (persistence/save-state! new-state))
+
+(def state-listeners
+  [notify-by-email! persist-new-state!])
+
+(defn on-app-state-change [_ _ old-state new-state]
+ (when (not= old-state new-state)
+   (doseq [f state-listeners]
+     (f old-state new-state))))
+
+
+;; other logic
+
+(defn initialize! []
+  (reset! app-state (or (persistence/load-state) default-state)))
+
+(defn register-listeners! []
+  (add-watch app-state :on-app-state-change on-app-state-change))
---
+++ test/web_watchdog/state_test.clj
@@ -0,0 +1,42 @@
+(ns web-watchdog.state-test
+  (:require [web-watchdog.state :refer :all]
+            [web-watchdog.test-utils :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest notify-by-email!-test
+  (let [sent-emails (atom nil)
+        setup!      (fn []
+                      (reset! sent-emails #{}))]
+    (with-redefs [postal.core/send-message
+                  (fn [{:keys [to]}] (swap! sent-emails into to))]
+      (testing "mail not sent if the content becomes available for the first time"
+        (let [old-state (set-sites default-state [(site "a")])
+              new-state (set-sites default-state [(site "a" :content-hash "a-hash")])]
+          (setup!)
+          (notify-by-email! old-state new-state)
+          (is (= #{} @sent-emails))))
+      (testing "mail sent if the content changes"
+        (let [old-state (set-sites default-state [(site "a" :content-hash "old-hash")])
+              new-state (set-sites default-state [(site "a" :content-hash "new-hash")])]
+          (setup!)
+          (notify-by-email! old-state new-state)
+          (is (= (set (site-emails "a")) @sent-emails))))
+      (testing "mail sent if site becomes unavailable"
+        (let [old-state (set-sites default-state [(site "a" :content-hash "constant")])
+              new-state (set-sites default-state [(site "a" :content-hash "constant" :fail-counter 1)])]
+          (setup!)
+          (notify-by-email! old-state new-state)
+          (is (= (set (site-emails "a")) @sent-emails))))
+      (testing "mail not sent if site stays unavailable"
+        (let [old-state (set-sites default-state [(site "a" :content-hash "constant" :fail-counter 1)])
+              new-state (set-sites default-state [(site "a" :content-hash "constant" :fail-counter 2)])]
+          (setup!)
+          (notify-by-email! old-state new-state)
+          (is (= #{} @sent-emails))))
+      (testing "mail not sent if site becomes available and content doesn't change"
+        (let [old-state (set-sites default-state [(site "a" :content-hash "constant" :fail-counter 20)])
+              new-state (set-sites default-state [(site "a" :content-hash "constant")])]
+          (setup!)
+          (notify-by-email! old-state new-state)
+          (is (= #{} @sent-emails)))))))
