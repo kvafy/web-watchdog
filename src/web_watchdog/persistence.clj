c76b278935f075fb88e050d5f722d32406e8f237
David Chaloupka <chaloupka.david@gmail.com>
2015-11-28 18:26:12 +0100

    Fixing a known injection vulnerability.

--- src/web_watchdog/persistence.clj
+++ src/web_watchdog/persistence.clj
@@ -1,11 +1,20 @@
-(ns web-watchdog.persistence)
+(ns web-watchdog.persistence
+  (:require [web-watchdog.utils :as utils]
+            [clojure.tools.reader.edn :as edn]))
 
 (def state-file "state.clj")
 
 (defn save-state! [state]
-  (spit state-file (pr-str state)))
+  (as-> state tmp
+        ; make sure to write plain Clojure data digestable by edn reader
+        (utils/update-map-keys tmp :re-pattern #(.pattern %))
+        (pr-str tmp)
+        (spit state-file tmp)))
 
 (defn load-state []
   (try
-    (read-string (slurp state-file))
+    (-> state-file
+        slurp
+        edn/read-string
+        (utils/update-map-keys :re-pattern #(re-pattern %)))
     (catch java.io.IOException ex nil)))
