140ba8722ffa9ac759b820ba144191e27d30a998
David Chaloupka <chaloupka.david@gmail.com>
2024-08-17 14:24:04 +0100

    Move ClojureScript dependencies to :provided profile to lean down the uberjar

--- project.clj
+++ project.clj
@@ -9,17 +9,11 @@
                  [clj-http "3.1.0"]
                  ; web server
                  [ring/ring-core "1.12.2"]
-                 [ring/ring-jetty-adapter "1.12.2"]
-                 [commons-io/commons-io "2.16.1"]  ;; required dep for ring-core
-                 [compojure "1.7.1"]
                  [ring/ring-defaults "0.5.0"]
+                 [ring/ring-jetty-adapter "1.12.2"]
                  [ring/ring-json "0.5.1"]
-                 ; web client
-                 [org.clojure/clojurescript "1.11.132"]
-                 [reagent "1.2.0"]
-                 [cljsjs/react "18.2.0-1"]     ;; required by reagent
-                 [cljsjs/react-dom "18.2.0-1"] ;; required by reagent
-                 ]
+                 [commons-io/commons-io "2.16.1"]  ;; required dep for ring-core
+                 [compojure "1.7.1"]]
   :plugins [[lein-ring "0.12.6"]
             [lein-cljsbuild "1.1.8"]]
   :cljsbuild
@@ -31,6 +25,13 @@
            :externs ["resources/externs.js"]}}]}
   :ring {:handler web-watchdog.handler/app}
   :profiles
-    {:dev
+    {:provided
+     {:dependencies [[org.clojure/clojurescript "1.11.132"]
+                     [reagent "1.2.0"]
+                     [cljsjs/react "18.2.0-1"]     ;; required by reagent
+                     [cljsjs/react-dom "18.2.0-1"] ;; required by reagent
+                     ]}
+     :dev
       {:dependencies [[javax.servlet/servlet-api "2.5"]
+                      [ring/ring-devel "1.9.4"]
                       [ring/ring-mock "0.4.0"]]}})
