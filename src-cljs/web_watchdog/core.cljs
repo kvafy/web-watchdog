cd0efc62d8d753944932fcf75d6f9f7d8db5a7ad
David Chaloupka <chaloupka.david@gmail.com>
2024-09-08 17:47:57 +0100

    Hide a Popover when clicked somewhere outside of it

--- src-cljs/web_watchdog/core.cljs
+++ src-cljs/web_watchdog/core.cljs
@@ -9,14 +9,23 @@
   (let [container (.getElementById js/document "content")]
     (reagent.dom/render (components/content) container)))
 
-(defn init-bootstrap! []
-  ; Initialize Bootstrap Popover plug-in (opt-in).
-  ; Must exploit event delegation and pass in the selector,
-  ; otherwise popups would work only for elements that are
-  ; in the HTML at the time of calling this function, but
-  ; not for elements added later.
-  (-> (js/$ "body")
-      (.popover #js {:selector "[data-bs-toggle='popover']"})))
+(defn init-bootstrap!
+  "Initialize the Bootstrap Popover plug-in (opt-in)."
+  []
+  (let [body (js/$ "body")
+        popover-selector "[data-bs-toggle='popover']"]
+    ; Must exploit event delegation and pass in the selector,
+    ; otherwise popups would work only for elements that are
+    ; in the HTML at the time of calling this function, but
+    ; not for elements added later by Reagent.
+    (.popover body #js {:selector popover-selector})
+
+    ; Custom function to hide a popover when clicked outside of it.
+    ; Unfortunately Bootstrap supports this natively only for <a>
+    ; elements.
+    (.on body "click" (fn []
+                        (-> (js/$ "[data-bs-toggle='popover']")
+                            (.popover "hide"))))))
 
 (defn init-state-refresh! []
   ; Periodically poll for current state.
