6f93937dcae010131359ae48de3f68896e160091
David Chaloupka <chaloupka.david@gmail.com>
2024-12-28 14:03:36 +0000

    Adjust handling of cookies and add support for custom per-site authentication
    Somewhat confusingly, when a site misses certain authentication cookies,
    it may manifest as "Unsupported or unrecognized SSL message" error on
    download.

--- src/web_watchdog/networking.clj
+++ src/web_watchdog/networking.clj
@@ -1,20 +1,25 @@
 (ns web-watchdog.networking
   (:require [clj-http.client :as client]
+            [clj-http.cookies]
             [integrant.core :as ig]
             [web-watchdog.utils :as utils]))
+
+(defn authenticate-site [url opts]
+  )
 
 (defn download
   "Downloads contents of the given URL.
 
    Returns a 2-tuple `[body ex-info]` in which exactly one item is non-nil."
   [url]
-  (let [opts {:insecure? true  ;; accept self-signed SSL certs
+  (let [cookie-store (clj-http.cookies/cookie-store)
+        opts {:insecure? true  ;; accept self-signed SSL certs
+              :cookie-store cookie-store
               :headers
               ;; Avoid 403 from certain sites.
-              {"User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"}}
-        cm (java.net.CookieManager.)]
-    (java.net.CookieHandler/setDefault cm)
+              {"User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"}}]
     (try
+      (authenticate-site url opts)
       (let [ok-response (client/get url opts)]
         [(:body ok-response) nil])
       (catch java.lang.Exception thrown
